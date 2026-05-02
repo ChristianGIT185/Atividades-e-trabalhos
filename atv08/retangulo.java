717beab4369bde563fdc2cd8dfe5236cab020fc3
Christian/ <moiak900@gmail.com>
2026-05-01 23:14:24 -0300

    Fix class implementation for Retangulo

---
+++ atv08/retangulo.java
@@ -0,0 +1,19 @@
+public class Retangulo implements atv08 {
+    private double base;
+    private double altura;
+
+    public Retangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public int quantidadeArestas() {
+        return 4;
+    }
+
+    @Override
+    public double calculaArea() {
+        return base * altura;
+    }
+}
